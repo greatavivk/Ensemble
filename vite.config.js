3eef25249ce4cac0f75e28aa8220df576f4e1d52
greatavivk <gretkakar@gmail.com>
2025-08-13 11:46:57 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-createRoot(document.getElementById("root")).render(<App />);
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
+});
