f13656d603807917a6fcada7083c0df41b2b7af0
greatavivk <gretkakar@gmail.com>
2025-08-13 11:40:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If plugin-react isn't installed, remove it and this import,
-// but it's nice to have for JSX fast refresh locally.
 export default defineConfig({
   plugins: [react()],
   build: { outDir: 'dist' }
